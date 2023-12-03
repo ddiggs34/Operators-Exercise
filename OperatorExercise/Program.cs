1717053916a93743798b54394f2be536b036802a
David Diggs <ddiggs34@gmail.com>
2023-12-02 21:24:39 -0500

    done

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,41 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //EXERCISE 1
+            int a = 3;
+            int b = 4;
+
+            var sum = a + b;
+
+            var sub = a - b;
+
+            var quotient = a * b;
+
+            var remainder = b % a;
+
+            Console.WriteLine($"{sum} is the sum");
+            Console.WriteLine($"{sub} is the sub");
+            Console.WriteLine($"{quotient} is the qoutient");
+            Console.WriteLine($"{remainder} is the remainder from a / b");
+
+            //EXERCISE
+
+            Console.WriteLine("We're getting the area of your circle. What is the radius?");
+            var radius = double.Parse(Console.ReadLine());
+
+            double area = AreaOfCirle(radius);
+
+            Console.WriteLine($"The area of your circle with a radius of {radius} is {area} ");
+
+
         }
+
+        public static double AreaOfCirle(double radius)
+        {
+            return (Math.PI * radius * radius);
+            }
     }
 }
